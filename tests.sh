7803ab19d0b328034a2008f865fb7e31276fa2a1
Emil Ingerslev <emil@ingerslev.io>
2018-09-06 14:13:18 +0200

    Refactor shell tests to shunit2

--- tests.sh
+++ tests.sh
@@ -2,32 +2,33 @@
 
 go build
 
-local failed_tests=0
-
-function test {
-  local bold=$'\e[01m'
-  local green=$'\e[32m'
-  local red=$'\e[31m'
-  local reset=$'\e[00m'
-
-  local testname=$1
-  shift
-  echo -n "├─ ${bold}$testname$reset"
+function assertRun() {
   local result
   result=$(./shuttle "$@" 2>&1)
   local result_status=$?
-  if [[ $result_status -eq 0 ]]; then
-    echo -e "\e[0K\r├─ ${bold}$testname ── ${green}success$reset"
-  else
-    failed_tests=$((failed_tests + 1))
-    echo -e "\e[0K\r${bold}${red}├─ ${bold}${red}$testname ── failed $reset"
-    echo "$result" | sed "s/^/${bold}${red}│$reset /"
-    echo "${bold}${red}├───────────────────────────────────────────────────────$reset"
+  if [[ $result_status -gt 0 ]]; then
+    fail "Status code was $result_status\n$result"
   fi
 }
 
-echo "Running tests:"
-test 'can run build' -p examples/moon-base run build test
-test 'cant run undefined' -p examples/moon-bitch run build test
+function assertErrorCode() {
+  local expectedErrorCode=$1
+  shift
+  local result
+  result=$(./shuttle "$@" 2>&1)
+  local result_status=$?
+  if [[ $result_status -ne $expectedErrorCode ]]; then
+    fail "Status code wasn't $expectedErrorCode but $result_status\nOutput: $result"
+  fi
+}
 
-exit $failed_tests
+test_moon_base_builds() {
+  assertRun -p examples/moon-base run build test
+}
+
+test_venus_base_fails() {
+  assertErrorCode 2 -p examples/venus-base run build test
+}
+
+# Load and run shUnit2.
+. ./shunit2
