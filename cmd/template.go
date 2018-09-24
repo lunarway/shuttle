11b2239220b7680971a9ceb4e72874dbbd37b0e9
Emil Ingerslev <emil@ingerslev.io>
2018-09-24 20:52:12 +0200

    Only output templates using "-o" and support templates all the places
    Templates will be looked up in
    - project/templates/
    - project/
    - plan/templates/
    - plan/

--- cmd/template.go
+++ cmd/template.go
@@ -16,6 +16,7 @@ type context struct {
 	Args map[string]string
 }
 
+var output string
 var templateCmd = &cobra.Command{
 	Use:   "template [template]",
 	Short: "Execute a template",
@@ -29,37 +30,69 @@ var templateCmd = &cobra.Command{
 			parts := strings.SplitN(arg, "=", 2)
 			namedArgs[parts[0]] = parts[1]
 		}
-		templateDir := path.Join(projectContext.LocalPlanPath, "templates")
 
-		templatePath := path.Join(templateDir, templateName)
+		templatePath := resolveFirstPath([]string{
+			path.Join(projectContext.ProjectPath, "templates", templateName),
+			path.Join(projectContext.ProjectPath, templateName),
+			path.Join(projectContext.LocalPlanPath, "templates", templateName),
+			path.Join(projectContext.LocalPlanPath, templateName),
+		})
+		if templatePath == "" {
+			panic(fmt.Sprintf("Could not find a template named `%s`", templateName))
+		}
 
 		tmpl, err := template.New(templateName).Funcs(tmplFuncs.GetFuncMap()).ParseFiles(templatePath)
 
 		if err != nil {
 			panic(err)
 		}
-		// TODO: This is probably not the right place to initialize the temp dir?
-		os.Mkdir(projectContext.TempDirectoryPath, 0755)
 
-		outputYamlFilename := strings.Replace(templateName, "tmpl", "yaml", -1)
-
-		fmt.Printf("Template generated: .shuttle/temp/%s\n", outputYamlFilename)
-		file, err := os.Create(path.Join(projectContext.TempDirectoryPath, outputYamlFilename))
-
-		if err != nil {
-			panic(err)
-		}
-
-		err = tmpl.Execute(file, context{
+		context := context{
 			Args: namedArgs,
 			Vars: projectContext.Config.Variables,
-		})
-		if err != nil {
-			panic(err)
+		}
+
+		if output == "" {
+			err = tmpl.Execute(os.Stdout, context)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			// TODO: This is probably not the right place to initialize the temp dir?
+			os.MkdirAll(projectContext.TempDirectoryPath, os.ModePerm)
+
+			file, err := os.Create(path.Join(projectContext.TempDirectoryPath, output))
+			if err != nil {
+				panic(err)
+			}
+
+			err = tmpl.Execute(file, context)
+			if err != nil {
+				panic(err)
+			}
 		}
 	},
 }
 
 func init() {
+	templateCmd.Flags().StringVarP(&output, "output", "o", "", "Select filename to output file to in temporary directory")
 	rootCmd.AddCommand(templateCmd)
+}
+
+func resolveFirstPath(paths []string) string {
+	for _, templatePath := range paths {
+		if fileAvailable(templatePath) {
+			return templatePath
+		}
+	}
+	return ""
+}
+
+func fileAvailable(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
 }
