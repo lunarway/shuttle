90da7bcd36a0d2c296ccbcf23261cca6a16fb907
Emil Ingerslev <emil@ingerslev.io>
2018-10-01 08:39:49 +0200

    Add simple `prepare` command
    Command doesn't do much, but is intended for fx CI to be run
    before anything else, ensuring fx git plan are fetched.
    It's expected that this command will do more in the future

---
+++ cmd/prepare.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	prepareCmd = &cobra.Command{
+		Use:   "prepare",
+		Short: "Load external resources",
+		Long:  `Load external resources as a preparation step, before starting to use shuttle`,
+		Run: func(cmd *cobra.Command, args []string) {
+			getProjectContext()
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(prepareCmd)
+}
