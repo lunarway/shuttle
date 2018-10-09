878b86b82f4977f42e9ad6cee6a531e39db7581b
Emil Ingerslev <emil@ingerslev.io>
2018-10-09 15:04:23 +0200

    test

--- pkg/templates/funcs.go
+++ pkg/templates/funcs.go
@@ -6,6 +6,7 @@ import (
 	"text/template"
 
 	"github.com/Masterminds/sprig"
+	yaml "gopkg.in/yaml.v2"
 )
 
 type KeyValuePair struct {
@@ -127,6 +128,38 @@ func TmplIs(a interface{}, b interface{}) bool {
 
 func TmplIsnt(a interface{}, b interface{}) bool {
 	return a != b
+}
+
+// ToYaml takes an interface, marshals it to yaml, and returns a string. It will
+// always return a string, even on marshal error (empty string).
+//
+// This is designed to be called from a template.
+//
+// Borrowed from github.com/helm/helm/pkg/chartutil
+func ToYaml(v interface{}) string {
+	data, err := yaml.Marshal(v)
+	if err != nil {
+		// Swallow errors inside of a template.
+		return ""
+	}
+	return string(data)
+}
+
+// FromYaml converts a YAML document into a map[string]interface{}.
+//
+// This is not a general-purpose YAML parser, and will not parse all valid
+// YAML documents. Additionally, because its intended use is within templates
+// it tolerates errors. It will insert the returned error message string into
+// m["Error"] in the returned map.
+//
+// Borrowed from github.com/helm/helm/pkg/chartutil
+func FromYaml(str string) map[string]interface{} {
+	m := map[string]interface{}{}
+
+	if err := yaml.Unmarshal([]byte(str), &m); err != nil {
+		m["Error"] = err.Error()
+	}
+	return m
 }
 
 // TODO: Add description
