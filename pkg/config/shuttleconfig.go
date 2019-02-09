f0d575451cdb886b2bbec40e56148762af2f5325
Crevil <bjoern.soerensen@gmail.com>
2019-02-09 12:23:24 +0100

    Handle missing and invalid shuttle configuration gracefully
    The current implementation panics and logs a stack trace along with a
    rather hard to read error message.
    
    This change reports if the file is missing or invalid along with some
    guidance on what to do next.
    
    Related to #25

--- pkg/config/shuttleconfig.go
+++ pkg/config/shuttleconfig.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
@@ -35,7 +34,7 @@ type ShuttleProjectContext struct {
 
 // Setup the ShuttleProjectContext for a specific path
 func (c *ShuttleProjectContext) Setup(projectPath string, uii ui.UI, clean bool, skipGitPlanPulling bool) *ShuttleProjectContext {
-	c.Config.getConf(projectPath)
+	c.Config.getConf(uii, projectPath)
 	c.UI = uii
 	c.ProjectPath = projectPath
 	c.LocalShuttleDirectoryPath = path.Join(c.ProjectPath, ".shuttle")
@@ -60,18 +59,18 @@ func (c *ShuttleProjectContext) Setup(projectPath string, uii ui.UI, clean bool,
 }
 
 // getConf loads the ShuttleConfig from yaml file in the project path
-func (c *ShuttleConfig) getConf(projectPath string) *ShuttleConfig {
+func (c *ShuttleConfig) getConf(uii ui.UI, projectPath string) *ShuttleConfig {
 	var configPath = path.Join(projectPath, "shuttle.yaml")
 
 	//log.Printf("configpath: %s", configPath)
 
 	yamlFile, err := ioutil.ReadFile(configPath)
 	if err != nil {
-		panic(fmt.Sprintf("yamlFile.Get err   #%v ", err))
+		uii.ExitWithErrorCode(2, "Failed to load shuttle configuration: %s\n\nMake sure you are in a project using shuttle and that a 'shuttle.yaml' file is available.", err)
 	}
 	err = yaml.Unmarshal(yamlFile, c)
 	if err != nil {
-		panic(fmt.Sprintf("Unmarshal: %v", err))
+		uii.ExitWithErrorCode(2, "Failed to parse shuttle configuration: %s\n\nMake sure your 'shuttle.yaml' is valid.", err)
 	}
 
 	switch c.PlanRaw {
