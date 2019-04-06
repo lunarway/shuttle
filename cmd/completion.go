e31fca968af782ccbc8b5852b4020c6d331b2d03
Bjørn Sørensen <bso@lunarway.com>
2019-04-06 21:59:49 +0200

    Use kubectl zsh completion wrapper

--- cmd/completion.go
+++ cmd/completion.go
@@ -15,6 +15,8 @@
 package cmd
 
 import (
+	"bytes"
+	"io"
 	"os"
 
 	"fmt"
@@ -32,9 +34,6 @@ var completionCmd = &cobra.Command{
 	Short: "Generates shell completion scripts",
 	Long: `Output shell completion code for the specified shell (bash or zsh). The shell code must be evaluated to provide
 interactive completion of shuttle commands. This can be done by sourcing it from the .bash _profile.
-
-Detailed instructions on how to do this are available here:
-https://kubernetes.io/docs/tasks/tools/install-shuttle/#enabling-shell-autocompletion
 
 Note for zsh users: [1] zsh completions are only supported in versions of zsh >= 5.2
 
@@ -75,7 +74,7 @@ Examples:
 		uii = uii.SetContext(ui.LevelSilent)
 		switch args[0] {
 		case "zsh":
-			rootCmd.GenZshCompletion(os.Stdout)
+			runCompletionZsh(os.Stdout, rootCmd)
 		case "bash":
 			rootCmd.GenBashCompletion(os.Stdout)
 		default:
@@ -85,13 +84,153 @@ Examples:
 
 func init() {
 	rootCmd.AddCommand(completionCmd)
-	// Here you will define your flags and configuration settings.
+}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// completionCmd.PersistentFlags().String("foo", "", "A help for foo")
+// this writes a zsh competion file that wraps the bash completion file
+//
+// Copied from kubectl: https://github.com/kubernetes/kubernetes/blob/9c2df998af9eb565f11d42725dc77e9266483ffc/pkg/kubectl/cmd/completion/completion.go#L145
+func runCompletionZsh(out io.Writer, shuttle *cobra.Command) error {
+	zshHead := "#compdef shuttle\n"
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// completionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	out.Write([]byte(zshHead))
+
+	zshInitialization := `
+__shuttle_bash_source() {
+	alias shopt=':'
+	alias _expand=_bash_expand
+	alias _complete=_bash_comp
+	emulate -L sh
+	setopt kshglob noshglob braceexpand
+	source "$@"
+}
+__shuttle_type() {
+	# -t is not supported by zsh
+	if [ "$1" == "-t" ]; then
+		shift
+		# fake Bash 4 to disable "complete -o nospace". Instead
+		# "compopt +-o nospace" is used in the code to toggle trailing
+		# spaces. We don't support that, but leave trailing spaces on
+		# all the time
+		if [ "$1" = "__shuttle_compopt" ]; then
+			echo builtin
+			return 0
+		fi
+	fi
+	type "$@"
+}
+__shuttle_compgen() {
+	local completions w
+	completions=( $(compgen "$@") ) || return $?
+	# filter by given word as prefix
+	while [[ "$1" = -* && "$1" != -- ]]; do
+		shift
+		shift
+	done
+	if [[ "$1" == -- ]]; then
+		shift
+	fi
+	for w in "${completions[@]}"; do
+		if [[ "${w}" = "$1"* ]]; then
+			echo "${w}"
+		fi
+	done
+}
+__shuttle_compopt() {
+	true # don't do anything. Not supported by bashcompinit in zsh
+}
+__shuttle_ltrim_colon_completions()
+{
+	if [[ "$1" == *:* && "$COMP_WORDBREAKS" == *:* ]]; then
+		# Remove colon-word prefix from COMPREPLY items
+		local colon_word=${1%${1##*:}}
+		local i=${#COMPREPLY[*]}
+		while [[ $((--i)) -ge 0 ]]; do
+			COMPREPLY[$i]=${COMPREPLY[$i]#"$colon_word"}
+		done
+	fi
+}
+__shuttle_get_comp_words_by_ref() {
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[${COMP_CWORD}-1]}"
+	words=("${COMP_WORDS[@]}")
+	cword=("${COMP_CWORD[@]}")
+}
+__shuttle_filedir() {
+	local RET OLD_IFS w qw
+	__shuttle_debug "_filedir $@ cur=$cur"
+	if [[ "$1" = \~* ]]; then
+		# somehow does not work. Maybe, zsh does not call this at all
+		eval echo "$1"
+		return 0
+	fi
+	OLD_IFS="$IFS"
+	IFS=$'\n'
+	if [ "$1" = "-d" ]; then
+		shift
+		RET=( $(compgen -d) )
+	else
+		RET=( $(compgen -f) )
+	fi
+	IFS="$OLD_IFS"
+	IFS="," __shuttle_debug "RET=${RET[@]} len=${#RET[@]}"
+	for w in ${RET[@]}; do
+		if [[ ! "${w}" = "${cur}"* ]]; then
+			continue
+		fi
+		if eval "[[ \"\${w}\" = *.$1 || -d \"\${w}\" ]]"; then
+			qw="$(__shuttle_quote "${w}")"
+			if [ -d "${w}" ]; then
+				COMPREPLY+=("${qw}/")
+			else
+				COMPREPLY+=("${qw}")
+			fi
+		fi
+	done
+}
+__shuttle_quote() {
+    if [[ $1 == \'* || $1 == \"* ]]; then
+        # Leave out first character
+        printf %q "${1:1}"
+    else
+	printf %q "$1"
+    fi
+}
+autoload -U +X bashcompinit && bashcompinit
+# use word boundary patterns for BSD or GNU sed
+LWORD='[[:<:]]'
+RWORD='[[:>:]]'
+if sed --help 2>&1 | grep -q GNU; then
+	LWORD='\<'
+	RWORD='\>'
+fi
+__shuttle_convert_bash_to_zsh() {
+	sed \
+	-e 's/declare -F/whence -w/' \
+	-e 's/_get_comp_words_by_ref "\$@"/_get_comp_words_by_ref "\$*"/' \
+	-e 's/local \([a-zA-Z0-9_]*\)=/local \1; \1=/' \
+	-e 's/flags+=("\(--.*\)=")/flags+=("\1"); two_word_flags+=("\1")/' \
+	-e 's/must_have_one_flag+=("\(--.*\)=")/must_have_one_flag+=("\1")/' \
+	-e "s/${LWORD}_filedir${RWORD}/__shuttle_filedir/g" \
+	-e "s/${LWORD}_get_comp_words_by_ref${RWORD}/__shuttle_get_comp_words_by_ref/g" \
+	-e "s/${LWORD}__ltrim_colon_completions${RWORD}/__shuttle_ltrim_colon_completions/g" \
+	-e "s/${LWORD}compgen${RWORD}/__shuttle_compgen/g" \
+	-e "s/${LWORD}compopt${RWORD}/__shuttle_compopt/g" \
+	-e "s/${LWORD}declare${RWORD}/builtin declare/g" \
+	-e "s/\\\$(type${RWORD}/\$(__shuttle_type/g" \
+	<<'BASH_COMPLETION_EOF'
+`
+	out.Write([]byte(zshInitialization))
+
+	buf := new(bytes.Buffer)
+	shuttle.GenBashCompletion(buf)
+	out.Write(buf.Bytes())
+
+	zshTail := `
+BASH_COMPLETION_EOF
+}
+__shuttle_bash_source <(__shuttle_convert_bash_to_zsh)
+_complete shuttle 2>/dev/null
+`
+	out.Write([]byte(zshTail))
+	return nil
 }
