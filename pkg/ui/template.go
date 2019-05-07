6ba74677dd7cea5913b63865dbbc1a6d23cd5a5a
Bjørn <bso@lunarway.com>
2019-05-07 11:10:52 +0200

    Expose template functions in template flags (#41)
    This aligns available template functions between template flags and the
    template command.

--- pkg/ui/template.go
+++ pkg/ui/template.go
@@ -2,37 +2,18 @@ package ui
 
 import (
 	"fmt"
-	"html/template"
 	"io"
-	"strings"
-)
+	"text/template"
 
-var lsTemplateFuncs = template.FuncMap{
-	"trim":       strings.TrimSpace,
-	"upperFirst": upperFirst,
-	"rightPad":   rightPad,
-}
+	"github.com/lunarway/shuttle/pkg/templates"
+)
 
 func Template(destination io.Writer, name, text string, data interface{}) error {
 	t := template.New(name)
-	t.Funcs(lsTemplateFuncs)
+	t.Funcs(templates.GetFuncMap())
 	t, err := t.Parse(text)
 	if err != nil {
 		return fmt.Errorf("invalid template: %v", err)
 	}
 	return t.Execute(destination, data)
-}
-
-// rightPad adds padding to the right of a string.
-func rightPad(s string, padding int) string {
-	template := fmt.Sprintf("%%-%ds", padding)
-	return fmt.Sprintf(template, s)
-}
-
-// upperFirst upper cases the first letter in the string
-func upperFirst(s string) string {
-	if len(s) <= 1 {
-		return strings.ToUpper(s)
-	}
-	return fmt.Sprintf("%s%s", strings.ToUpper(s[0:1]), s[1:])
 }
