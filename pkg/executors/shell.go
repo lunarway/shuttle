207e23f840c95a9a9a7517e260263ca37366bb5d
Bjørn Sørensen <bso@lunar.app>
2021-08-31 14:23:14 +0200

    Handle large stdout and stderr lines from scripts
    Currently if a script outputs a single line to stdout or stderr over 16k
    characters the script is stopped with a pipe error. When used in combination
    with shell scripts this makes for a very confusing error state where output is
    just limited or the process either hangs or fails without explanation.
    
    This change replaces the shell executer based on github.com/go-cmd/cmd with one
    using the standard libraries os/exec package. Further more the line buffer limit
    is increased to 512k characters.
    
    go-cmd does not support changing the maximum line length so the library cannot
    support our use case.
    
    This bug was found when a Go service was testing an HTML template. The test
    would output the rendered HTML template and a complex SVG exceeded the current
    16k limit. This resulted in the test being marked as successfull by the Go
    toolchain but as the stdout pipe was broken shell scripts would not work as
    intended.

--- pkg/executors/shell.go
+++ pkg/executors/shell.go
@@ -1,33 +1,25 @@
 package executors
 
 import (
+	"bufio"
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/lunarway/shuttle/pkg/config"
-	"github.com/lunarway/shuttle/pkg/errors"
-
-	go_cmd "github.com/go-cmd/cmd"
+	shuttleerrors "github.com/lunarway/shuttle/pkg/errors"
 )
 
 // Build builds the docker image from a shuttle plan
 func executeShell(ctx context.Context, context ActionExecutionContext) error {
-	//log.Printf("Exec: %s", context.Action.Shell)
-	//cmdAndArgs := strings.Split(s.Shell, " ")
-	//cmd := cmdAndArgs[0]
-	//args := cmdAndArgs[1:]
 	shuttlePath, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 
-	cmdOptions := go_cmd.Options{
-		Buffered:  false,
-		Streaming: true,
-	}
+	execCmd := exec.CommandContext(ctx, "sh", "-c", fmt.Sprintf("cd '%s'; %s", context.ScriptContext.Project.ProjectPath, context.Action.Shell))
 
-	execCmd := go_cmd.NewCmdOptions(cmdOptions, "sh", "-c", "cd '"+context.ScriptContext.Project.ProjectPath+"'; "+context.Action.Shell)
-
-	//execCmd := exec.Command("sh", "-c", context.Action.Shell)
 	execCmd.Env = os.Environ()
 	for name, value := range context.ScriptContext.Args {
 		execCmd.Env = append(execCmd.Env, fmt.Sprintf("%s=%s", name, value))
@@ -39,51 +31,51 @@ func executeShell(ctx context.Context, context ActionExecutionContext) error {
 	execCmd.Env = append(execCmd.Env, fmt.Sprintf("PATH=%s", shuttlePath+string(os.PathListSeparator)+os.Getenv("PATH")))
 	execCmd.Env = append(execCmd.Env, fmt.Sprintf("SHUTTLE_PLANS_ALREADY_VALIDATED=%s", context.ScriptContext.Project.LocalPlanPath))
 
-	doneChan := make(chan struct{})
-	go func() {
-		defer close(doneChan)
-		for execCmd.Stdout != nil || execCmd.Stderr != nil {
-			select {
-			case line, open := <-execCmd.Stdout:
-				if !open {
-					execCmd.Stdout = nil
-					continue
-				}
-				context.ScriptContext.Project.UI.Infoln("%s", line)
-			case line, open := <-execCmd.Stderr:
-				if !open {
-					execCmd.Stderr = nil
-					continue
-				}
-				context.ScriptContext.Project.UI.Errorln("%s", line)
+	stdout, err := execCmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("get stdout pipe: %w", err)
+	}
+
+	stderr, err := execCmd.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("get stderr pipe: %w", err)
+	}
+
+	err = execCmd.Start()
+	if err != nil {
+		return fmt.Errorf("start command: %w", err)
+	}
+
+	stdScanner(stdout, context.ScriptContext.Project.UI.Infoln)
+	stdScanner(stderr, context.ScriptContext.Project.UI.Errorln)
+
+	err = execCmd.Wait()
+	if err != nil {
+		var exitError *exec.ExitError
+		if errors.As(err, &exitError) {
+			exitCode := exitError.ExitCode()
+			if exitCode > 0 {
+				return shuttleerrors.NewExitCode(4, "Failed executing script `%s`: shell script `%s`\nExit code: %v", context.ScriptContext.ScriptName, context.Action.Shell, exitCode)
 			}
 		}
-	}()
+		return err
+	}
 
-	// Run and wait for Cmd to return, discard Status
-	context.ScriptContext.Project.UI.Titleln("shell: %s", context.Action.Shell)
+	return nil
+}
 
-	// stop cmd if context is cancelled
-	go func() {
-		select {
-		case <-ctx.Done():
-			err := execCmd.Stop()
-			if err != nil {
-				context.ScriptContext.Project.UI.Errorln("Failed to stop script '%s': %v", context.Action.Shell, err)
-			}
-		case <-doneChan:
-		}
-	}()
+func stdScanner(reader io.ReadCloser, output func(format string, args ...interface{})) {
+	scanner := bufio.NewScanner(reader)
 
-	select {
-	case status := <-execCmd.Start():
-		<-doneChan
-		if status.Exit > 0 {
-			return errors.NewExitCode(4, "Failed executing script `%s`: shell script `%s`\nExit code: %v", context.ScriptContext.ScriptName, context.Action.Shell, status.Exit)
-		}
-		return nil
-	case <-ctx.Done():
-		return ctx.Err()
+	// increase max line capacity of the buffer to allow for reading very long
+	// lines. This sets the maximum to 512k characters per line.
+	const maxCapacity = 512 * 1024
+	buf := make([]byte, maxCapacity)
+	scanner.Buffer(buf, maxCapacity)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		output("%s", line)
 	}
 }
 
