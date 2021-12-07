f4d296edc1c798f1ce895a175f5e565e49c0b73e
Bjørn <bso@lunar.app>
2021-12-07 10:18:24 +0100

    Fail template rendering on template func errors (#73)
    Currently when using template functions toYaml, fromYaml and getFileContent
    fails silently or in odd ways where the rendered output contains error
    information. Go's template engine supports template functions that returns
    errors and thus will propagate errors correctly. This can help identify falty
    paths and contents in a clear way instead of silently doing nothing.
    
    The change is motivated by fighting with understanding why getFileContent does
    nothing in some cases. As the error is hidden there are no hints as to what is
    going on.
    
    This change adds error return values to the functions along with adds some tests
    of this behaviour. The functions are considered safe to change as it is unusual
    to use them without expeting failures on bad input, eg. read a non-existing
    file. Function getFiles is not changed to return an error as it is expected that
    a non-existing or empty directory will return no files. A test is added to lock
    this behaviour.

---
+++ pkg/sdk/template_test.go
@@ -0,0 +1,71 @@
+package sdk
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRenderTemplate(t *testing.T) {
+	tt := []struct {
+		name         string
+		templatePath string
+		output       string
+		templateCtx  TemplateContext
+		err          error
+	}{
+		{
+			name:         "valid",
+			templatePath: "testdata/valid.yaml",
+			templateCtx: TemplateContext{
+				Vars: map[string]string{
+					"bar": "bar",
+				},
+			},
+			output: `foo: bar
+`,
+			err: nil,
+		},
+		{
+			name:         "read content of unknown file",
+			templatePath: "testdata/get_file_content.yaml",
+			templateCtx: TemplateContext{
+				Vars: map[string]string{
+					"path": "unknown.yaml",
+				},
+			},
+			output: ``,
+			err:    errors.New("template: get_file_content.yaml:2:3: executing \"get_file_content.yaml\" at <getFileContent (string \"path\" .Vars)>: error calling getFileContent: open unknown.yaml: no such file or directory"),
+		},
+		{
+			name:         "read content of file",
+			templatePath: "testdata/get_file_content.yaml",
+			templateCtx: TemplateContext{
+				Vars: map[string]string{
+					"path": "testdata/valid.yaml",
+				},
+			},
+			output: `bar: baz
+foo: {{ get "bar" .Vars }}
+
+`,
+			err: nil,
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			var output bytes.Buffer
+			err := renderTemplate(tc.templatePath, tc.name, &output, tc.templateCtx, "{{", "}}")
+
+			if tc.err != nil {
+				assert.EqualError(t, err, tc.err.Error())
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, tc.output, output.String())
+		})
+	}
+}
