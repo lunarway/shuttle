23c8fabe7a23e309a14d496d25848ea41e093a43
hhoegh <her@lunar.app>
2022-01-14 12:07:14 +0100

    added test for has and split up logic

--- cmd/has_test.go
+++ cmd/has_test.go
@@ -2,41 +2,62 @@ package cmd
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHasNoErr(t *testing.T) {
+type testCase struct {
+	name      string
+	input     []string
+	stdoutput string
+	erroutput string
+	err       error
+}
+
+func TestHas(t *testing.T) {
 	strings := func(s ...string) []string {
 		return s
 	}
-	tt := []struct {
-		name   string
-		input  []string
-		output string
-	}{
+
+	testCases := []testCase{
 		{
-			name:   "has variable",
-			input:  strings("-p", "../examples/repo-project", "has", "docker.baseImage"),
-			output: "",
+			name:      "has variable",
+			input:     strings("-p", "../examples/repo-project", "has", "docker.baseImage"),
+			stdoutput: "",
+			erroutput: "",
+			err:       nil,
+		},
+		{
+			name:      "has wrong argument switch",
+			input:     strings("-j", "../examples/repo-project", "has", "docker.baseImage"),
+			stdoutput: "",
+			erroutput: "",
+			err:       fmt.Errorf("unknown shorthand flag: 'j' in -j"),
 		},
 	}
+	executeTestCases(t, testCases)
+}
 
-	for _, tc := range tt {
+func executeTestCases(t *testing.T, testCases []testCase) {
+	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			buf := new(bytes.Buffer)
-			rootCmd.SetOut(buf)
-			rootCmd.SetErr(buf)
+			stdBuf := new(bytes.Buffer)
+			errBuf := new(bytes.Buffer)
+
+			rootCmd.SetOut(stdBuf)
+			rootCmd.SetErr(errBuf)
 			rootCmd.SetArgs(tc.input)
+
 			err := rootCmd.Execute()
-
-			assert.NoError(t, err)
-
-			if tc.output != "" {
-				assert.Equal(t, tc.output, buf.String())
+			if tc.err == nil {
+				assert.NoError(t, err)
+			} else {
+				assert.EqualError(t, tc.err, err.Error())
 			}
-
+			assert.Equal(t, tc.stdoutput, stdBuf.String())
+			assert.Equal(t, tc.erroutput, errBuf.String())
 		})
 	}
 }
