b19a53ead56cfa52e1f9e7bff69c1032e6b42d5b
Bjørn Sørensen <bso@lunar.app>
2022-03-07 09:10:26 +0100

    Replace duplicate strings function with args func
    An args test helper function was introduced in
    99d93c84e139df28235b87315359a87693c4e7f7 (#94) but all tests were not migrated
    to use it in that change.
    
    This change updates the test code removing the rest of the strings functions and
    use the shared args.

--- cmd/ls_test.go
+++ cmd/ls_test.go
@@ -5,20 +5,17 @@ import (
 )
 
 func TestLs(t *testing.T) {
-	strings := func(s ...string) []string {
-		return s
-	}
 	testCases := []testCase{
 		{
 			name:      "list one action",
-			input:     strings("-p", "../examples/no-plan-project", "ls"),
+			input:     args("-p", "../examples/no-plan-project", "ls"),
 			stdoutput: "Available Scripts:\n  hello        \n",
 			erroutput: "",
 			err:       nil,
 		},
 		{
 			name:      "list actions",
-			input:     strings("-p", "../examples/repo-project/", "ls"),
+			input:     args("-p", "../examples/repo-project/", "ls"),
 			stdoutput: "Pulling latest plan changes on master\nAvailable Scripts:\n  build        Build the docker image\n  deploy       Deploys the image to a kubernetes environment\n  push         Push the docker image\n  say          Say something\n  test         Run test for the project\n",
 			erroutput: "",
 			err:       nil,
