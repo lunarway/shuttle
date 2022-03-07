f442bd4e6b525b09eac0de3f73a3cfff3988d364
Bjørn Sørensen <bso@lunar.app>
2022-03-07 13:37:47 +0100

    Migrate template accept tests from shunit2 to Go
    This change contains the migration of the template command's acceptance tests
    from bash and shunit2 to Go.

---
+++ cmd/template_test.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func TestTemplate(t *testing.T) {
+	t.Cleanup(func() {
+		removeShuttleDirectories(t)
+	})
+
+	testCases := []testCase{
+		{
+			name:  "local path",
+			input: args("-p", "testdata/project", "template", "../custom-template.tmpl", "GO_VERSION=1.17"),
+			stdoutput: `# Custom docker file template not located inside a project
+FROM golang:1.17-alpine
+`,
+			erroutput: "",
+			err:       nil,
+		},
+		{
+			name:  "alternative delimiters",
+			input: args("-p", "testdata/project", "template", "../custom-template-alternative-delims.tmpl", "--delims", ">>,<<"),
+			stdoutput: `FROM golang:1.17-alpine
+LABEL svc=shuttle
+`,
+			erroutput: "",
+			err:       nil,
+		},
+	}
+	executeTestCases(t, testCases)
+}
