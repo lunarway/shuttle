53b56dbb9a2c2d22b249aa168ab0cbec032fe565
Bjørn <bso@lunar.app>
2022-03-07 09:09:22 +0100

    Migrate plan accept tests from shunit2 to Go (#95)
    This change contains the migration of the plan command's acceptance tests from
    bash and shunit2 to Go.

---
+++ cmd/plan_test.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func TestPlan(t *testing.T) {
+	testCases := []testCase{
+		{
+			name:      "no plan",
+			input:     args("-p", "testdata/project", "plan"),
+			stdoutput: "",
+			erroutput: "",
+			err:       nil,
+		},
+		{
+			name:      "git plan",
+			input:     args("-p", "testdata/project-git", "plan"),
+			stdoutput: "https://github.com/lunarway/shuttle-example-go-plan.git",
+			erroutput: "",
+			err:       nil,
+		},
+		{
+			name:      "no plan with template",
+			input:     args("-p", "testdata/project", "plan", "--template", "{{.PlanRaw}}"),
+			stdoutput: "false",
+			erroutput: "",
+			err:       nil,
+		},
+	}
+	executeTestCases(t, testCases)
+}
