66483864f9d969eb20b8cf228e1adf41217160e4
Bjørn <bso@lunar.app>
2022-03-08 09:02:52 +0100

    Migrate template accept tests from shunit2 to Go (#101)

--- tests.sh
+++ tests.sh
@@ -8,25 +8,6 @@ if [[ $buildExitCode -ne 0 ]]; then
   exit $buildExitCode
 fi
 
-function assertErrorCode() {
-  local expectedErrorCode=$1
-  shift
-  result=$(./shuttle "$@" 2>&1)
-  result_status=$?
-  if [[ $result_status -ne $expectedErrorCode ]]; then
-    fail "Status code wasn't $expectedErrorCode but $result_status\nOutput: $result"
-  fi
-}
-
-test_template_local_path() {
-  assertErrorCode 0 -p examples/moon-base template ../custom-template.tmpl -o Dockerfile-custom GO_VERSION=1.16
-}
-
-test_template_local_path_alternate_delims() {
-  result=$(./shuttle -p examples/moon-base template ../custom-template-alternate-delims.tmpl --delims ">>,<<")
-  assertEquals "FROM earth-united/moon-base" "$result"
-}
-
 test_run_sub_dir_say() {
   result=$(cd examples/stepping-stone/sub-dir && ./../../../shuttle run say)
   pwd=$(pwd)
