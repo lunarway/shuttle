ca62b3bcc12e855ec5e8b3501b4d5e53add44fbd
Bjørn Sørensen <bso@lunar.app>
2022-11-19 13:27:10 +0100

    Remove debug log

--- cmd/config.go
+++ cmd/config.go
@@ -37,7 +37,6 @@ func newConfig(uii *ui.UI, contextProvder contextProvider) *cobra.Command {
 			if parsedPlan.IsGitPlan {
 				fmt.Fprintf(cmd.OutOrStdout(), "Plan:\n%v %v", context.Config.Plan, parsedPlan.Head)
 			} else {
-				fmt.Printf("%+v\n", context.Config)
 				plan := context.Config.Plan
 				if plan == "" {
 					plan = "false"
