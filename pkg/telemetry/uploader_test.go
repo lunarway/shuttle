d25dbb182cf530e38a0d8cb6734a1c5ab158769f
Kasper J. Hermansen <kah@lunar.app>
2023-07-17 12:40:09 +0200

    feat: with clean up function
    Signed-off-by: Kasper J. Hermansen <kah@lunar.app>

--- pkg/telemetry/uploader_test.go
+++ pkg/telemetry/uploader_test.go
@@ -2,6 +2,10 @@ package telemetry
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
 	"testing"
 	"time"
 
@@ -71,21 +75,93 @@ func TestTelemetryUploader(t *testing.T) {
 
 		uploader := NewTelemetryUploader(
 			"some-url",
-			WithUploadFunction(
-				func(ctx context.Context, url string, event []UploadTraceEvent) error {
-					assert.Equal(t, "some-url", url)
-					assert.NotEmpty(t, event)
+			WithUploadFunction(func(ctx context.Context, url string, event []UploadTraceEvent) error {
+				assert.Equal(t, "some-url", url)
+				assert.NotEmpty(t, event)
 
-					return nil
-				},
-			),
+				return nil
+			}),
 			WithGetTelemetryFiles(func(ctx context.Context, location string) ([]string, error) {
 				return getTelemetryFiles(ctx, "testdata/full-upload-test")
 			}),
-			WithCleanUp(false),
-		)
+			WithCleanUp(false))
 
 		err := uploader.Upload(context.Background())
 		require.NoError(t, err)
 	})
+
+	t.Run("endpoint not available", func(t *testing.T) {
+		t.Parallel()
+
+		ctx := context.Background()
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
+		defer cancel()
+
+		ok, err := availabilityCheck(ctx, "http://some-url-which-doesnt-exist")
+		assert.NoError(t, err)
+		assert.False(t, ok)
+	})
+
+	t.Run("endpoint available bad response", func(t *testing.T) {
+		t.Parallel()
+
+		server := startServer(
+			t,
+			func() (string, func(w http.ResponseWriter, r *http.Request)) {
+				return "/available", func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(200)
+				}
+			},
+		)
+		defer server.Close()
+
+		ctx := context.Background()
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
+		defer cancel()
+
+		ok, err := availabilityCheck(ctx, fmt.Sprintf("http://%s/available", server.Addr))
+		require.NoError(t, err)
+		assert.True(t, ok)
+	})
+}
+
+type serverFunc = func() (string, func(w http.ResponseWriter, r *http.Request))
+
+func startServer(t *testing.T, serverFuncs ...serverFunc) *http.Server {
+	t.Helper()
+	server := &http.Server{}
+
+	// Create a new mux for handling requests
+	mux := http.NewServeMux()
+
+	for _, f := range serverFuncs {
+		path, f := f()
+		mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("handling request for path: %s", path)
+			f(w, r)
+		})
+	}
+
+	server.Handler = mux
+
+	// Start the server with a dynamically allocated port
+	listener, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("Failed to start server: %v", err)
+	}
+
+	// Set the listener address to the server
+	server.Addr = listener.Addr().String()
+
+	// Start serving requests
+	go func(t *testing.T) {
+		err := server.Serve(listener)
+		if err != nil && err != http.ErrServerClosed {
+			t.Fatalf("Server error: %v", err)
+		}
+	}(t)
+
+	time.Sleep(time.Millisecond * 20)
+
+	return server
 }
