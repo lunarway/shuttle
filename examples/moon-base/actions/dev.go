c58b511773e0c79173dd923f38662fc9b5431376
Kasper Juul Hermansen <kah@lunar.app>
2023-10-30 09:25:36 +0100

    fix: golang actions now work in go workspaces (#190)
    Previously if a go.work (workspaces) exist, the build fails to compile, this is because we cannot do a proper build without the dependencies being shared between all the modules.
    
    This sets GOWORK=off for only the internal builds
    
    Signed-off-by: Kasper J. Hermansen <kah@lunar.app>

--- examples/moon-base/actions/dev.go
+++ examples/moon-base/actions/dev.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	_ "github.com/lunarway/shuttle"
 )
 
 func Dev(ctx context.Context) error {
--- pkg/executors/golang/codegen/compilation.go
+++ pkg/executors/golang/codegen/compilation.go
@@ -2,6 +2,8 @@ package codegen
 
 import (
 	"context"
+	"log"
+	"os"
 	"os/exec"
 	"path"
 
@@ -10,11 +12,15 @@ import (
 
 func CompileBinary(ctx context.Context, ui *ui.UI, shuttlelocaldir string) (string, error) {
 	cmd := exec.Command("go", "build")
+	cmd.Env = os.Environ()
+	// We need to set workspaces off, as we don't want users to have to add the golang modules to their go.work
+	cmd.Env = append(cmd.Env, "GOWORK=off")
+
 	cmd.Dir = path.Join(shuttlelocaldir, "tmp")
 
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		ui.Verboseln("compile-binary output: %s", string(output))
+		log.Printf("compile-binary output: %s", string(output))
 		return "", err
 	}
 
