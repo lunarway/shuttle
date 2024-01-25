7347f6250caeceb25b48b6023dfdb4a854c57b34
Kasper J. Hermansen <contact@kjuulh.io>
2024-01-25 18:25:42 +0100

    "feat(extensions): add install extensions from registry
    This pr includes everything up to actually downloading the extension from the upstream registry
    
    Such as pulling an example registry entry
    
    ```json
    {
      "name": "SuperExtender",
      "description": "Enhances your experience with superpowers.",
      "version": "1.0.3",
      "downloadUrls": [
        {
          "architecture": "amd64",
          "os": "linux",
          "url": "https://example.com/linux/amd64/superextender.tar.gz",
          "checksum": "efgh5678"
        },
        {
          "architecture": "x86",
          "os": "windows",
          "url": "https://example.com/windows/x86/superextender.zip",
          "checksum": "ijkl91011"
        },
        {
          "architecture": "arm64",
          "os": "darwin",
          "url": "https://example.com/windows/x86/superextender.zip",
          "checksum": "ijkl91011"
        }
      ]
    }
    ```
    
    Which will match on the current os using $GOOS and $GOARCH set by golang itself. We may however have to depend on some other variables if go is not installed by I believe it should work anyway"

--- internal/extensions/extension.go
+++ internal/extensions/extension.go
@@ -1,4 +1,68 @@
 package extensions
 
+import (
+	"context"
+	"fmt"
+	"path"
+	"runtime"
+
+	"github.com/lunarway/shuttle/internal/global"
+)
+
 // Extension is the descriptor of a single extension, it is used to add description to the cli, as well as calling the specific extension in question
-type Extension struct{}
+type Extension struct {
+	os          string
+	arch        string
+	globalStore *global.GlobalStore
+	remote      *registryExtension
+}
+
+func newExtensionFromRegistry(globalStore *global.GlobalStore, registryExtension *registryExtension) (*Extension, error) {
+	os := runtime.GOOS
+	arch := runtime.GOARCH
+
+	return &Extension{
+		os:          os,
+		arch:        arch,
+		globalStore: globalStore,
+		remote:      registryExtension,
+	}, nil
+
+}
+
+func (e *Extension) Ensure(ctx context.Context) error {
+	extensionsCachePath := getExtensionsCachePath(e.globalStore)
+	binaryName := e.getExtensionBinaryName()
+	if err := ensureExists(extensionsCachePath); err != nil {
+		return fmt.Errorf("failed to create cache path: %w", err)
+	}
+
+	binaryPath := path.Join(extensionsCachePath, binaryName)
+	if exists(binaryPath) {
+		return nil
+	}
+
+	downloadLink := e.getRemoteBinaryDownloadLink()
+	if downloadLink == nil {
+		return fmt.Errorf("failed to find a valid extension matching your os and architecture")
+	}
+
+	//TODO: Initiate download
+
+	panic("not implemented yet")
+}
+
+func (e *Extension) getExtensionBinaryName() string {
+	return e.remote.Name
+}
+
+func (e *Extension) getRemoteBinaryDownloadLink() *registryExtensionDownloadLink {
+	for _, download := range e.remote.DownloadUrls {
+		if download.Os == e.os &&
+			download.Architecture == e.arch {
+			return &download
+		}
+	}
+
+	return nil
+}
