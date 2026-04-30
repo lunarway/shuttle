c20c99c687e557f9ff1fdc9c0a1f1f0b1ad93c85
Lunar Renovate <115620512+lunar-renovate@users.noreply.github.com>
2026-04-30 09:48:52 +0200

    chore(deps): update golang docker tag to v1.26.1 (#293)
    automated merge

--- pkg/executors/golang/compile/compile.go
+++ pkg/executors/golang/compile/compile.go
@@ -228,7 +228,7 @@ func goInstalled() bool {
 func getGolangImage() string {
 	const (
 		// renovate: datasource=docker depName=golang
-		golangImageVersion = "1.26.0"
+		golangImageVersion = "1.26.1"
 	)
 
 	golangImage := fmt.Sprintf("golang:%s", golangImageVersion)
